be2b25bda346a3c0c488978afc30b88b8041b143
Petr Trukan <pitter@narod.ru>
2017-02-04 01:22:36 +0500

    update constructor Interpretator()

--- G-code Viewer/src/gui/MainWindow.java
+++ G-code Viewer/src/gui/MainWindow.java
@@ -6,10 +6,13 @@ import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
+import logic.Interpretator;
+
 public class MainWindow {
 
 	private static int groupWidthLeftText = 250;
 	private static int widthText = 20;
+	private static Label labTraceBounds;
 
 	public static void main(String[] args) {
 		Display display = new Display();
@@ -17,29 +20,36 @@ public class MainWindow {
 		shell.setText("G-code Viewer");
 		shell.setImage(new Image(display, "main.png"));
 		shell.setMinimumSize(850, 450);
-
+		
 		Group forText = new Group(shell, SWT.BORDER);
-		forText.setLayout(new RowLayout());
+		forText.setLayout(new GridLayout());
 		forText.setBounds(0, 0, groupWidthLeftText + 10, shell.getSize().y - 10);
 		Group forTrace = new Group(shell, SWT.BORDER);
-
 		forTrace.setBounds(forText.getBounds().width, 0, shell.getSize().x - forText.getBounds().width - 10,
 				shell.getSize().y - 10);
+		
+		labTraceBounds = new Label(forTrace,SWT.NONE);
+		labTraceBounds.setText(".");
+		labTraceBounds.setBounds(10, 5, forTrace.getClientArea().width-15,20);
+		
 		Composite leftTop = new Composite(forTrace, SWT.BORDER);
-		leftTop.setBounds(3, 30, (forTrace.getClientArea().width - 6) / 2, (forTrace.getClientArea().height - 30) / 2);
+		leftTop.setBounds(3, 30, (forTrace.getClientArea().width - 6) * 3 / 8, (forTrace.getClientArea().height - 30) * 3 / 8);
 
 		Composite leftDown = new Composite(forTrace, SWT.BORDER);
-		leftDown.setBounds(3, 30 + (forTrace.getClientArea().height - 30) / 2, (forTrace.getClientArea().width - 6) / 2,
-				(forTrace.getClientArea().height - 30) / 2);
+		leftDown.setBounds(3, 30 + (forTrace.getClientArea().height - 30) * 3 / 8, (forTrace.getClientArea().width - 6) * 3 / 8,
+				(forTrace.getClientArea().height - 30) * 5 / 8);
 
 		Composite rightTop = new Composite(forTrace, SWT.BORDER);
-		rightTop.setBounds(3 + (forTrace.getClientArea().width - 6) / 2, 30, (forTrace.getClientArea().width - 6) / 2,
-				(forTrace.getClientArea().height - 30) / 2);
+		rightTop.setBounds(3 + (forTrace.getClientArea().width - 6) * 3 / 8, 30, (forTrace.getClientArea().width - 6) * 5 / 8,
+				(forTrace.getClientArea().height - 30) * 3 / 8);
 
 		Composite rightDown = new Composite(forTrace, SWT.BORDER);
-		rightDown.setBounds(3 + (forTrace.getClientArea().width - 6) / 2,
-				30 + (forTrace.getClientArea().height - 30) / 2, (forTrace.getClientArea().width - 6) / 2,
-				(forTrace.getClientArea().height - 30) / 2);
+		rightDown.setBounds(3 + (forTrace.getClientArea().width - 6) * 3 / 8,
+				30 + (forTrace.getClientArea().height - 30) * 3 / 8, (forTrace.getClientArea().width - 6) * 5 / 8,
+				(forTrace.getClientArea().height - 30) * 5 / 8);
+		
+		//initialize empty data
+		Interpretator intr=new Interpretator(); 
 
 		Label labBeginMargin = new Label(forText, SWT.SIMPLE);
 		labBeginMargin.setBounds(2, 1, groupWidthLeftText, widthText);
@@ -60,33 +70,50 @@ public class MainWindow {
 		labCode.setText("Исследуемый G-code:");
 
 		Text textCode = new Text(forText, SWT.LEFT | SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
-		textCode.setBounds(10, 10 + 5 * widthText, groupWidthLeftText - 50, shell.getSize().y - 9 * widthText);
+		textCode.setBounds(10, 10 + 6 * widthText, groupWidthLeftText - 50, shell.getSize().y - 10 * widthText);
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		Menu bar = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(bar);
 		MenuItem loadBut = new MenuItem(bar, SWT.PUSH);
 		Image image = new Image(display, "load.ICO");
 		loadBut.setImage(image);
 		loadBut.setAccelerator(SWT.MOD1 + 'S');
+		loadBut.addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event arg0) {
+				FileDialog dialog=new FileDialog(shell, SWT.OPEN);
+				dialog.setFilterNames(new String[] {"tap type files (.tap)","all type files"});
+				dialog.setFilterExtensions(new String[] {"*.tap","**.*"});
+				String filePath=dialog.open();
+				if(filePath!=null) intr.parseCode(filePath);
+			}
+		});
 
 		shell.addControlListener(new ControlAdapter() {
 			public void controlResized(ControlEvent e) {
 				forText.setBounds(0, 0, groupWidthLeftText, shell.getSize().y - 50);
 				forTrace.setBounds(forText.getBounds().width, 0, shell.getSize().x - forText.getBounds().width - 10,
 						shell.getSize().y - 50);
-				textCode.setBounds(10, 10 + 5 * widthText, groupWidthLeftText - 7, shell.getSize().y - 9 * widthText);
-				leftTop.setBounds(3, 30, (forTrace.getClientArea().width - 6) / 2,
-						(forTrace.getClientArea().height - 30) / 2);
-
-				leftDown.setBounds(3, 30 + (forTrace.getClientArea().height - 30) / 2,
-						(forTrace.getClientArea().width - 6) / 2, (forTrace.getClientArea().height - 30) / 2);
-
-				rightTop.setBounds(3 + (forTrace.getClientArea().width - 6) / 2, 30,
-						(forTrace.getClientArea().width - 6) / 2, (forTrace.getClientArea().height - 30) / 2);
-
-				rightDown.setBounds(3 + (forTrace.getClientArea().width - 6) / 2,
-						30 + (forTrace.getClientArea().height - 30) / 2, (forTrace.getClientArea().width - 6) / 2,
-						(forTrace.getClientArea().height - 30) / 2);
+				textCode.setBounds(10, 10 + 6 * widthText, groupWidthLeftText - 7, shell.getSize().y - 10 * widthText);
+				
+				labTraceBounds.setBounds(10, 5, forTrace.getClientArea().width-15,20);
+				leftTop.setBounds(3, 30, (forTrace.getClientArea().width - 6) * 3 / 8, (forTrace.getClientArea().height - 30) * 3 / 8);
+				leftDown.setBounds(3, 30 + (forTrace.getClientArea().height - 30) * 3 / 8, (forTrace.getClientArea().width - 6) * 3 / 8,
+						(forTrace.getClientArea().height - 30) * 5 / 8);
+				rightTop.setBounds(3 + (forTrace.getClientArea().width - 6) * 3 / 8, 30, (forTrace.getClientArea().width - 6) * 5 / 8,
+						(forTrace.getClientArea().height - 30) * 3 / 8);
+				rightDown.setBounds(3 + (forTrace.getClientArea().width - 6) * 3 / 8,
+						30 + (forTrace.getClientArea().height - 30) * 3 / 8, (forTrace.getClientArea().width - 6) * 5 / 8,
+						(forTrace.getClientArea().height - 30) * 5 / 8);
 
 			}
 		});
@@ -98,6 +125,12 @@ public class MainWindow {
 				display.sleep();
 		}
 		display.dispose();
+	}
+
+	
+	
+	public static void setTextTrace(String text){
+		labTraceBounds.setText(text);
 	}
 
 }
--- G-code Viewer/src/logic/Interpretator.java
+++ G-code Viewer/src/logic/Interpretator.java
@@ -1,5 +1,78 @@
 package logic;
 
-public class Interpretator {
+import org.eclipse.swt.graphics.Point;
 
+import gui.MainWindow;
+
+public class Interpretator {
+	String filePath;
+	String code;
+	String startLine;
+	String endLine;
+	String textCode;
+
+	Point[] pathLeftTop;
+	Point[] pathLeftDown;
+	Point[] pathRightTop;
+	Point[] pathRightDown;
+
+	public Interpretator() {
+		filePath = null;
+		code = null;
+		startLine = "";
+		endLine = "";
+		textCode = "";
+		MainWindow.setTextTrace("\t x = 0...0 \t\t\t y = 0...0 \t\t\t z = 0...0");
+		pathLeftTop = new Point[8];
+		pathLeftDown = new Point[8];
+		pathRightTop = new Point[8];
+		pathRightDown = new Point[8];
+		for (int i = 0; i < 9; i++) {
+			if (i % 2 == 0) {
+				pathLeftTop[i] = new Point(0, 0);
+				pathLeftDown[i] = new Point(0, 0);
+				pathRightTop[i] = new Point(0, 0);
+				pathRightDown[i] = new Point(0, 0);
+			} else {
+				if (i == 1 | i == 3) {
+					pathLeftTop[i] = new Point(0, (i - 2) * 5);
+					pathLeftDown[i] = new Point(0, (i - 2) * 5);
+					pathRightTop[i] = new Point(0, (i - 2) * 5);
+					pathRightDown[i] = new Point(0, (i - 2) * 5);
+				}
+				if (i == 5 | i == 7) {
+					pathLeftTop[i] = new Point((i - 6) * 5, 0);
+					pathLeftDown[i] = new Point((i - 6) * 5, 0);
+					pathRightTop[i] = new Point((i - 6) * 5, 0);
+					pathRightDown[i] = new Point((i - 6) * 5, 0);
+				}
+			}
+		}
+
+	}
+
+	public Point[] getPathLeftTop() {
+		return pathLeftTop;
+
+	}
+
+	public Point[] getPathLeftDown() {
+		return pathLeftDown;
+
+	}
+
+	public Point[] getPathRightTop() {
+		return pathRightTop;
+
+	}
+
+	public Point[] getPathRightDown() {
+		return pathRightDown;
+
+	}
+
+	public void parseCode(String fPath) {
+		filePath = fPath;
+
+	}
 }
